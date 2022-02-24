3fdc57d91eef1f9698e767f21121c8fa8f71143b
Mutato115 <75532023+Mutato115@users.noreply.github.com>
2022-02-24 12:31:04 +0100

    Added a multiblock redstone output
    By right-clicking a barrel in a multiblock, all the subbarrels output a redstone signal of power 15, which decreases during the next 3 seconds.

--- src/main/java/mutato115/mods/hgmrs/blocks/BlockBarrel.java
+++ src/main/java/mutato115/mods/hgmrs/blocks/BlockBarrel.java
@@ -1,10 +1,14 @@
 package mutato115.mods.hgmrs.blocks;
 
+import java.util.Random;
+
 import mutato115.mods.hgmrs.tiles.TeBarrel;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockBarrel extends BlockContainer {
@@ -22,5 +26,42 @@ public class BlockBarrel extends BlockContainer {
 	public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_) {
 		return new TeBarrel();
 	}
+	
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+		if (world.getTileEntity(x, y, z) instanceof TeBarrel) {
+			TeBarrel barrel = (TeBarrel) world.getTileEntity(x, y, z);
+			
+			barrel.setPowerForAll(15, world);
+			world.notifyBlocksOfNeighborChange(x, y, z, this);
+		}
+
+		return true;
+	}
+	
+
+	@Override
+	public boolean canProvidePower() {
+		return true;
+	}
+	
+	@Override
+	public int isProvidingWeakPower(IBlockAccess iba, int x, int y, int z, int meta) {
+        if (iba.getTileEntity(x, y, z) instanceof TeBarrel) {
+        	return ((TeBarrel) iba.getTileEntity(x, y, z)).getProvidedPower();
+        } else {
+        	return 0;
+        }
+    }
+
+ 
+	@Override
+    public int isProvidingStrongPower(IBlockAccess iba, int x, int y, int z, int meta) {
+		if (iba.getTileEntity(x, y, z) instanceof TeBarrel) {
+        	return ((TeBarrel) iba.getTileEntity(x, y, z)).getProvidedPower();
+        } else {
+        	return 0;
+        }
+    }
 
 }
--- src/main/java/mutato115/mods/hgmrs/tiles/TeBarrel.java
+++ src/main/java/mutato115/mods/hgmrs/tiles/TeBarrel.java
@@ -1,12 +1,15 @@
 package mutato115.mods.hgmrs.tiles;
 
 import io.netty.buffer.ByteBuf;
+import mutato115.mods.hgmrs.json.UniquePosition;
 import mutato115.mods.hgmrs.lib.Log;
+import mutato115.mods.hgmrs.registries.BlockRegistry;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
@@ -14,6 +17,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class TeBarrel extends TileEntity {
@@ -21,17 +25,28 @@ public class TeBarrel extends TileEntity {
     private TeBarrel master;
     private boolean isMaster;
     private boolean firstRun = true;
+    
+    private int currentPower = 0;
+    private int timer = 0;
+    
+    private List<UniquePosition> rhythm;
+    private List<TeBarrel> multiBlockBarrels;
 
-    public boolean isMaster(){
+    @Override
+    public Block getBlockType() {
+    	return BlockRegistry.barrel;
+    }
+    
+    public boolean isMaster() {
         return isMaster;
     }
 
-    public TeBarrel getMaster(){
+    public TeBarrel getMaster() {
         initializeMultiblockIfNecessary();
         return master;
     }
 
-    private void setMaster(TeBarrel master, int barrels){
+    private void setMaster(TeBarrel master, int barrels) {
         this.master = master;
         boolean wasMaster = isMaster;
         isMaster = master == this;
@@ -43,16 +58,23 @@ public class TeBarrel extends TileEntity {
     }
 
     @Override
-    public void updateEntity(){
+    public void updateEntity() {
         super.updateEntity();
         if(firstRun) {
             initializeMultiblockIfNecessary();
             firstRun = false;
         }
+        
+        if (isMaster) {
+        	if (this.getTimer() % 4 == 0) {
+        		this.decreasePowerInMultiblock(worldObj);
+        	}
+        	this.decreaseTimerForAll();
+        }
     }
 
     @Override
-    public void invalidate(){
+    public void invalidate() {
         super.invalidate();
         for(ForgeDirection d : ForgeDirection.VALID_DIRECTIONS) {
             TileEntity te = worldObj.getTileEntity(xCoord + d.offsetX, yCoord + d.offsetY, zCoord + d.offsetZ);
@@ -63,7 +85,7 @@ public class TeBarrel extends TileEntity {
         }
     }
 
-    private void initializeMultiblockIfNecessary(){
+    private void initializeMultiblockIfNecessary() {
         if(master == null || master.isInvalid()) {
             List<TeBarrel> connectedBarrels = new ArrayList<TeBarrel>();
             Stack<TeBarrel> traversingBarrels = new Stack<TeBarrel>();
@@ -88,33 +110,104 @@ public class TeBarrel extends TileEntity {
             for(TeBarrel barrel : connectedBarrels) {
                 barrel.setMaster(master, connectedBarrels.size());
             }
+            master.setBarrels(new ArrayList<TeBarrel>(connectedBarrels));
         }
     }
 
-    public void onGuiButtonPress(int id){
+    public void onGuiButtonPress(int id) {
 
     }
 
-    public void writeToPacket(ByteBuf buf){
+    public void writeToPacket(ByteBuf buf) {
 
     }
 
-    public void readFromPacket(ByteBuf buf){
+    public void readFromPacket(ByteBuf buf) {
     	
     }
 
     @Override
-    public void readFromNBT(NBTTagCompound tag){
+    public void readFromNBT(NBTTagCompound tag) {
         super.readFromNBT(tag);
 
         isMaster = tag.getBoolean("isMaster");
     }
 
     @Override
-    public void writeToNBT(NBTTagCompound tag){
+    public void writeToNBT(NBTTagCompound tag) {
         super.writeToNBT(tag);
 
         tag.setBoolean("isMaster", isMaster);
+    }
+    
+    public List<UniquePosition> getRhythm() {
+    	return isMaster ? this.rhythm : getMaster().getRhythm();
+    }
+    
+    public List<TeBarrel> getBarrels() {
+    	return isMaster ? this.multiBlockBarrels : getMaster().getBarrels();
+    }
+    
+    public void setBarrels(List<TeBarrel> barrels) {
+    	this.multiBlockBarrels = barrels;
+    }
+    
+    public int getProvidedPower() {
+    	return currentPower;
+    }
+    
+    public void setProvidedPower(int power) {
+    	this.currentPower = power;
+    }
+    
+    public void setPowerForAll(int power, World world) {
+    	if (isMaster) {
+    		for (TeBarrel barrel : this.multiBlockBarrels) {
+    			barrel.setProvidedPower(power);
+    			world.notifyBlocksOfNeighborChange(barrel.xCoord, barrel.yCoord, barrel.zCoord, BlockRegistry.barrel);
+    		}
+    	} else {
+    		getMaster().setPowerForAll(power, world);
+    	}
+    }
+    
+    public void decreasePowerInMultiblock(World world) {
+    	if (this.getProvidedPower() > 0) {
+    		if (isMaster) {
+        		for (TeBarrel barrel : this.multiBlockBarrels) {
+        			barrel.decreasePower(world);
+        		}
+        	} else {
+        		getMaster().decreasePowerInMultiblock(world);
+        	}
+    	}
+    }
+    
+    private void decreasePower(World world) {
+    	this.currentPower--;
+    	world.notifyBlocksOfNeighborChange(this.xCoord, this.yCoord, this.zCoord, this.getBlockType());
+    }
+    
+    public int getTimer() {
+    	return this.timer;
+    }
+    
+    private void resetTimer() {
+    	this.timer = 60;
+    }
+    
+    private void decreaseTimerForAll() {
+    	if (isMaster) {
+    		for (TeBarrel barrel : this.multiBlockBarrels) {
+    			barrel.decreaseTimer();
+    		}
+    	} else {
+    		getMaster().decreaseTimerForAll();
+    	}
+    }
+    
+    private void decreaseTimer() {
+    	this.timer--;
     }
 
 }
